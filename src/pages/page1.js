4672c0078dc7296e10a38f97b581c7ec25c7d26b
GabrielleAlmeida12 <98052195+GabrielleAlmeida12@users.noreply.github.com>
2023-09-20 14:42:35 -0300

    Eduplan

---
+++ src/pages/page1.js
@@ -0,0 +1,72 @@
+// pages/page1.js
+
+import React, { useState } from 'react';
+import { useRouter } from 'next/router';
+import styles from '../../styles/Page1.module.css';
+
+const Page1 = () => {
+  const [materia, setMateria] = useState('');
+  const [studyEndDate, setStudyEndDate] = useState('');
+  const router = useRouter();
+
+  const handleNext = async () => {
+    if (!materia || !studyEndDate) {
+      alert('Por favor, preencha todos os campos.');
+      return;
+    }
+
+    const formattedEndDate = new Date(studyEndDate).toISOString();
+
+    const response = await fetch('/api/study-plans', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ materia, studyEndDate: formattedEndDate }),
+    });
+
+    if (response.ok) {
+      router.push('/page2');
+    } else {
+      alert('Ocorreu um erro ao armazenar os dados.');
+    }
+  };
+
+  return (
+    <div className={styles.background}>
+      <div className={styles.background2}>
+      <div className={styles.containerquest}>
+        <div className={styles.containerinputquest}>
+          <label htmlFor="materia">
+            Para quê você vai estudar?
+          </label>
+          <input
+            className={styles.inputquest}
+            type="text"
+            id="materia"
+            value={materia}
+            onChange={(e) => setMateria(e.target.value)}
+          />
+        </div>
+        <div className={styles.containerinputquest}>
+          <label htmlFor="studyEndDate">
+            Até quando vai seu plano de estudo?
+          </label>
+          <input
+            className={styles.inputquest}
+            type="date"
+            id="studyEndDate"
+            value={studyEndDate}
+            onChange={(e) => setStudyEndDate(e.target.value)}
+          />
+        </div>
+        <button className={styles.page1Button} onClick={handleNext}>
+          Próxima Página
+        </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Page1;
